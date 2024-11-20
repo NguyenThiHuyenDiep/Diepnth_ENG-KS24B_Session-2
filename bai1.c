ff646fb4d771c05608a5b97d589198c2652b33a1
NguyenThiHuyenDiep <Diepnd2006@gmail.com>
2024-11-20 20:29:02 +0700

    B1

---
+++ bai1.c
@@ -0,0 +1,8 @@
+// khai bao va khoi tao cac bien voi cac kieu du lieu co ban trong C
+int tuoi = 20;
+// bien tuoi co kdl so nguyen voi gia tri 18
+char toan = 'A';
+// bien toan co kdl ky tu voi gia tri A
+float pi = 3.14;
+//bien pi co kdl so thuc voi gia tri 3.14
+
