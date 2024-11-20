fd89b6e50348ddac4936695841111b3ac80416ce
NguyenThiHuyenDiep <Diepnd2006@gmail.com>
2024-11-20 20:29:24 +0700

    b2

---
+++ bai2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int num1, num2;
+    printf("nhap so thu nhat ");
+    scanf("%d", &num1);
+    printf("nhap so thu hai ");
+    scanf("%d", &num2);
+    int tong = num1 +num2;
+    printf("tong 2 so la : %d\n", tong);
+    return 0;
+    }
